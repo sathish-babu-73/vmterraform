cde512371b54976b3f24470127ae97f97e993a1d
SATHISH BABU S <98654301+sathish-babu-73@users.noreply.github.com>
2023-09-05 21:53:34 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install apache2 -y
+sudo yum install git -y
+sudo yum update -y
+sudo yum install apache2 -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
